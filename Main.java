263a1d977bff2a00d4c059be0976dc12ca651334
IM ORY <156199116+imorydeveloper@users.noreply.github.com>
2024-01-11 15:15:34 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+package Exam;
+public class Main {
+    public static void main(String[] args) {
+        Square square = new Square(5);
+        System.out.println("Area of the square: " + square.getArea());
+        System.out.println("Perimeter of the square: " + square.getPerimeter());
+    }
+}
---
+++ Square.java
@@ -0,0 +1,7 @@
+
+package Exam;
+public class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+}
